04c8fc2fef0b96ebd04c705aabc3fe48b9c93a1c
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-27 22:58:08 +0300

    аывавы

---
+++ js/news.js
@@ -0,0 +1,23 @@
+
+var innerContent = document.getElementsByClassName("ebaniy-neobema")[0]
+let repos = [
+    {
+        id: 1,
+        title: `27.04.2021 | Релиз скрипта "Подсчёт онлайна из логов" `,
+        desc: "Подробнее о скрипте - <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">перейти <i class=\"fas fa-check\" style=\"font-size: 12px; color: rgb(255, 7, 112);\"></i></a>",
+    },
+]
+
+if(innerContent) {
+    for (var i = 0; i < repos.length; i++) {
+        var reposObj = repos[i]
+        var reposStr = `<div class="repos-element" id="repos-${reposObj.id}">
+    <div class="info">
+        <p class="title">${reposObj.title}</p>
+        ${reposObj.desc.replace(/\n/g,"<br>")}
+    </div>
+    <a class="download" href="${reposObj.url}" download>Скачать</a>
+</div>`
+        innerContent.innerHTML += reposStr
+    }
+}
