63d5ef3f068c2fef2afa893d1c64db2e4a2c0439
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-27 23:03:24 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -4,17 +4,20 @@ let repos = [
     {
         id: 1,
         title: `27.04.2021 | Релиз скрипта "Подсчёт онлайна из логов" `,
-        desc: "Подробнее о скрипте - <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">перейти</i></a>",
+        desc: "Подробнее о скрипте - <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">перейти</a>",
     },
 ]
 
 if(innerContent) {
-    for (var i = 0; i < news.length; i++) {
-        var newsObj = news[i]
-        var newsStr = `<div class="news-element" id="news-${newsObj.id}">
-    <p class="title">${newsObj.title}</p>
-    ${newsObj.desc.replace(/\n/g,"<br>")}
+    for (var i = 0; i < repos.length; i++) {ы
+        var reposObj = repos[i]
+        var reposStr = `<div class="repos-element" id="repos-${reposObj.id}">
+    <div class="info">
+        <p class="title">${reposObj.title}</p>
+        ${reposObj.desc.replace(/\n/g,"<br>")}
+    </div>
+    <a class="download" href="${reposObj.url}" download>Скачать</a>
 </div>`
-        innerContent.innerHTML += newsStr
+        innerContent.innerHTML += reposStr
     }
 }
