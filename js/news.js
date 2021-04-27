5d6b13d64e3092cdeda582eb307061aad08acfda
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-27 23:19:26 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -7,3 +7,14 @@ let news = [
         desc: "Подробнее о скрипте / скачать - <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">перейти</a>",
     },
 ]
+
+if(innerContent) {
+    for (var i = 0; i < news.length; i++) {
+        var newsObj = news[i]
+        var newsStr = `<div class="news-element" id="news-${newsObj.id}">
+    <p class="title">${newsObj.title}</p>
+    ${newsObj.desc.replace(/\n/g,"<br>")}
+</div>`
+        innerContent.innerHTML += newsStr
+    }
+}
