49e7e066003796a7d623fa0723714229a4662d57
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-28 14:07:10 +0300

    Update developers.js

--- js/developers.js
+++ js/developers.js
@@ -3,8 +3,8 @@ var innerContent = document.getElementsByClassName("neebaniy-neobema")[0]
 let news = [
     {
         id: 1,
-        title: `Артём Епифанцев <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a> \n Способности писать на языках: `,
-        desc: "LUA, JS.",
+        title: `Артём Епифанцев <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a> `,
+        desc: "'<b>' +  Способности писать на языках:" + '</b>',
     },
 ]
 
