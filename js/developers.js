e9f415d49cefd7e1732d0468800590610716157d
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-28 14:23:05 +0300

    Update developers.js

--- js/developers.js
+++ js/developers.js
@@ -3,8 +3,8 @@ var innerContent = document.getElementsByClassName("neebaniy-neobema")[0]
 let news = [
     {
         id: 1,
-        title: `Артём Епифанцев <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a> `,
-        desc: '<b>' + "Способности писать на языках:" + '</b> \n LUA, JavaScript',
+        title: `Артём Епифанцев <a href=\"https://vk.com/j.rojers\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a> `,
+        desc: '<b>' + "Способности писать на языках:" + '</b> \n LUA, JS \n <b>' +  "О себе:" + '</b> \n Бывший администратор сервера Arizona | Red-Rock, основатель семьи <a href=\"discord.gg/bWqMJSUy3z" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">Rojers 💸 Squad</a>',
     },
 ]
 
