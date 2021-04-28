bec7a70ef27512b2f23af4209bb0cda8fded26ea
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-28 14:34:08 +0300

    Update developers.js

--- js/developers.js
+++ js/developers.js
@@ -4,7 +4,7 @@ let news = [
     {
         id: 1,
         title: `Артём Епифанцев <a href=\"https://vk.com/j.rojers\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a> `,
-        desc: '<b>' + "Can write scripts in:" + '</b> \n LUA, JS \n <b>' +  "About me" + '</b> \n Бывший администратор сервера Arizona | Red-Rock, основатель семьи <a href=\"discord.gg/bWqMJSUy3z" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">Rojers 💸 Squad</a>',
+        desc: '<b>' + "Умеет писать скрипты на:" + '</b> \n LUA, JS \n <b>' +  "О себе:" + '</b> \n Бывший администратор сервера Arizona | Red-Rock, основатель семьи <a href=\"discord.gg/bWqMJSUy3z" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">Rojers 💸 Squad</a> \n <b>' +  "Контакты" + '</b> \n  ',
     },
 ]
 
