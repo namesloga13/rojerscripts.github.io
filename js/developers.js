772a23595bec62d4b583ce0e1844e985153c7bfc
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-28 14:01:12 +0300

    я пидоапрс

---
+++ js/developers.js
@@ -0,0 +1,20 @@
+
+var innerContent = document.getElementsByClassName("neebaniy-neobema")[0]
+let news = [
+    {
+        id: 1,
+        title: `Артём Епифанцев <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a>" \n Способности писать на языках: `,
+        desc: "LUA, JS.",
+    },
+]
+
+if(innerContent) {
+    for (var i = 0; i < news.length; i++) {
+        var newsObj = news[i]
+        var newsStr = `<div class="news-element" id="news-${newsObj.id}">
+    <p class="title">${newsObj.title}</p>
+    ${newsObj.desc.replace(/\n/g,"<br>")}
+</div>`
+        innerContent.innerHTML += newsStr
+    }
+}
