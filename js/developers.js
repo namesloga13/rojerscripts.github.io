903989816b496a155dc61b65de60d83a714131a0
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-04-28 14:11:02 +0300

    Update developers.js

--- js/developers.js
+++ js/developers.js
@@ -4,7 +4,7 @@ let news = [
     {
         id: 1,
         title: `Артём Епифанцев <a href=\"https://rojerscripts.github.io/downloads\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">[VK]</a> `,
-        desc: '<b>' + "Способности писать на языках:" + '</b>',
+        desc: '<b>' + "Способности писать на языках:" + '</b> \n LUA, JavaScript',
     },
 ]
 
